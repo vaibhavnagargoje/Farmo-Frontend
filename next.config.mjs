6f62eb2e627aa329b0fbe9098bb7b00ba2fce51d
vaibhavnagargoje <vaibhavnagargoje381@gmail.com>
2026-03-03 23:18:57 +0530

    conf change for api

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["*.ngrok-free.app", "localhost", "127.0.0.1"],
+  allowedDevOrigins: ["*.ngrok-free.app", "localhost", "127.0.0.1","api.farmo.in"],
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+        remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "api.farmo.in",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        pathname: "/**",
+      }
+    ]
   },
 }
 
